0b6819b96f24754dc8c7a06fc941561ce17a983c
onavi001 <34868568+onavi001@users.noreply.github.com>
2023-09-07 15:54:52 -0600

    rollback background

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -30,7 +30,6 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ background: 'red' }}
         />
       </label>
 
